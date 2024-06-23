77f265f4404051cfcdbd8290af905d7287b7cea1
HectorOrtiz789 <hector.ortiz4@unmsm.edu.pe>
2024-06-23 12:12:07 -0500

    Fin Seccion 9

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -1,36 +1,101 @@
 const { response, request } = require('express');
+const bcryptjs = require('bcryptjs');
+const Usuario = require('../models/usuario');
+const { validationResult } = require('express-validator');
+const mongoose = require('mongoose');
 
-const usuariosGet = (req = request, res = response) => {
-    const { q, nombre = 'no envia', apikey } = req.query;
+const usuariosGet = async(req = request, res = response) => {
+    // const {q,nombre = 'no envia', apikey} = req.query;
+    const {limite = 5, desde = 0} = req.query; // indicamos que vamos a recibir un parametro: limite, con valor por defecto 5
+    const query = {estado: true};
+
+    const limiteNum = Number(limite);
+    const desdeNum = Number(desde);
+    
+
+    const [total, usuarios] = await Promise.all([
+        Usuario.countDocuments(query), // retorna total
+        Usuario.find(query) // retorna los usuarios
+            .skip(Number(desde))
+            .limit(Number(limite))
+    ]);
+
+    if (isNaN(limiteNum) || isNaN(desdeNum)) {
+        return res.status(400).json({
+            msg: "Los parámetros 'limite' y 'desde' deben ser números"
+        });
+    }
+    
     res.json({
-        msg: 'get API - controller',
-        q,
-        nombre,
-        apikey
+        total,
+        usuarios
     });
+    // encuentra desde al limite registros de la DB
+    /* const usuarios = await Usuario.find(query)
+        .skip(Number(desde))
+        .limit(Number(limite));
+    const total = await Usuario.countDocuments(query); */
 };
 
-const usuariosPut = (req, res = response) => {
-    const { id } = req.params; // params puede traer muchos datos.
+const usuariosPut = async (req, res = response) => {
+    const { id } = req.params; // Obtener el ID de los parámetros de la solicitud
+
+    // Validar si el ID es un ObjectId de MongoDB válido
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({
+            msg: 'El ID proporcionado no es válido' // Enviar un error si el ID no es válido
+        });
+    }
+
+    // Excluyo password, google y correo (no se actualizan) y todo lo demás lo almaceno en resto
+    const {_id,password, google, correo, ...resto } = req.body;
+
+    // Encriptar la contraseña en caso que venga en el body
+    if (password) {
+        const salt = bcryptjs.genSaltSync(); // Cantidad de vueltas que hará la encriptación por defecto es 10
+        resto.password = bcryptjs.hashSync(password, salt); // Encripta el password y lo guarda en resto
+    }
+
+    // Actualiza el registro: lo busca por id y actualiza con los valores de resto
+    const usuario = await Usuario.findByIdAndUpdate(id, resto, { new: true });
+
     res.json({
         msg: 'put API - controller',
-        id
+        usuario // Devuelve el usuario actualizado
     });
 };
 
 
-const usuariosPost = (req, res = response) => {
-    const { nombre, edad } = req.body;
+const usuariosPost = async (req, res = response) => {
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() });
+    }
+    const { nombre, correo, password, rol } = req.body;
+    const usuario = new Usuario({ nombre, correo, password, rol });
+
+    // encriptar la contraseña
+    const salt = bcryptjs.genSaltSync(); // cantidad de vueltas que hará la encriptación por defecto es 10
+    usuario.password = bcryptjs.hashSync(password, salt); // encripta el password
+
+    await usuario.save(); // esto es para grabar en BD
+
     res.json({
         msg: 'post API - controller',
-        nombre,
-        edad
+        usuario
     });
 };
 
-const usuariosDelete = (req, res = response) => {
+
+const usuariosDelete = async(req, res = response) => {
+    const {id} = req.params;
+    // borrado físico
+    // const usuario = await Usuario.findByIdAndDelete(id);
+    // borrado lógico
+    const usuario = await Usuario.findByIdAndUpdate(id, {estado: false});
+    
     res.json({
-        msg: 'delete API - controller'
+        usuario
     });
 };
 
@@ -40,7 +105,6 @@ const usuariosPatch = (req, res = response) => {
     });
 };
 
-// Se exporta un objeto ya que habrá muchas funciones
 module.exports = {
     usuariosGet,
     usuariosPut,
--- models/server.js
+++ models/server.js
@@ -1,34 +1,47 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
+
+this.port = process.env.PORT || 3000;
 
 class Server {
     constructor() {
         this.app = express();
         this.port = process.env.PORT;
         this.usuariosPath = '/api/usuarios';
+
+        // Conectar a base de datos
+        this.conectarDB();
+
         // Middleware
         this.middlewares();
+
         // Rutas de la aplicación
         this.routes();
     }
-    
-    // Método donde se define el middleware que publicará la carpeta public
-    middlewares(){
+
+    async conectarDB() {
+        await dbConnection();
+    }
+
+    middlewares() {
         // CORS
         this.app.use(cors());
-        // Lectura y parseo del body, recibe lo que se envía
+
+        // Lectura y parseo del body
         this.app.use(express.json());
+
         // Directorio público
         this.app.use(express.static('public'));
     }
-    
+
     routes() {
         this.app.use(this.usuariosPath, require('../routes/usuarios'));
-    }    
-       
+    }
+
     listen() {
         this.app.listen(this.port, () => {
-            console.log('Servidor corriendo en puerto ', this.port);
+            console.log('Servidor corriendo en puerto', this.port);
         });
     }
 }
